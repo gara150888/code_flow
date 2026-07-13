e34f994b1f56a9f07ea0d3da1075e0c0593477b4
gara150888o <devgogogogogo@gmail.com>
2026-07-13 16:58:51 +0530

    some fix

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,5 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { index, pgTableCreator } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `first_${name}`);
 
 export const posts = createTable(
